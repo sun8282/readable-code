13d66f69318d9e7a43a31027012766ed9adfef6a
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-02-17 15:29:10 +0900

    add: 02-17 SRP원칙

---
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -0,0 +1,12 @@
+package cleancode.minesweeper.tobe;
+
+public class GameApplication {
+
+
+    public static void main(String[] args) {
+        Minesweeper minesweeper = new Minesweeper();
+        minesweeper.run();
+    }
+}
+
+
