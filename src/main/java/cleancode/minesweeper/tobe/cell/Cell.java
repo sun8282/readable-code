250dbc9f85d2d65ab5bc95751e739e090b2c1eac
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-02-17 18:08:35 +0900

    add: 02-17 LSP 원칙

--- src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
@@ -1,21 +1,14 @@
 package cleancode.minesweeper.tobe.cell;
 
-public abstract class Cell2 {
-    private static final String FLAG_SIGN = "⚑";
-    private static final String LAND_MINE_SIGN = "☼";
-    private static final String EMPTY_SIGN = "■";
-    private static final String UNCHECKED_SIGN = "□";
+public abstract class Cell {
+    protected static final String FLAG_SIGN = "⚑";
+    protected static final String UNCHECKED_SIGN = "□";
 
-    private int nearbyLandMineCount;
-    private boolean isLandMine;
-    private boolean isFlagged;
-    private boolean isOpened;
+    protected boolean isFlagged;
+    protected boolean isOpened;
     //Cell이 가진 속성 : 근처 지뢰 숫자, 지뢰 여부
     //Cell의 상태 : 깃발 유무, 열렸다/닫혔다, 사용자가 확인함
 
-    public abstract void turnOnLandMine();
-
-    public abstract void updateNearbyLandMineCount(int count);
 
     public void flag() {
         this.isFlagged = true;
